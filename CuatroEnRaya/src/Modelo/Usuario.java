f5c15be503b02276e0dac128762f3a0572d39f2d
ignac <ignac@avion>
2024-03-18 23:53:27 +0100

    CAMBIAR JRE21 A JRE20 y añadir constructor y getters y setters

--- CuatroEnRaya/src/Modelo/Usuario.java
+++ CuatroEnRaya/src/Modelo/Usuario.java
@@ -2,4 +2,26 @@ package Modelo;
 
 public class Usuario {
 	private String usuario;
+	private int edad;
+	
+	public Usuario(String nombre, int edad) {
+		this.usuario = nombre;
+		this.edad = edad;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
